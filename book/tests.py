520ea350165a434f0d7edf98ac2e1bab1d0975bc
andela-eisaac <deejecks@live.com>
2016-09-14 18:19:01 +0100

    [test] Test Book and Category models

--- book/tests.py
+++ book/tests.py
@@ -1,5 +1,7 @@
 from django.test import TestCase
 
+from .models import Book, Category
+
 
 class RandomTestCase(TestCase):
     def setUp(self):
@@ -10,3 +12,37 @@ class RandomTestCase(TestCase):
 
     def test_a_is_2(self):
         self.assertEqual(self.a, 2)
+
+
+class CategoryTestCase(TestCase):
+    def setUp(self):
+        self.category = Category.objects.create(name="test",)
+
+    def tearDown(self):
+        del self.category
+
+    def test_category_instance(self):
+        self.assertIsInstance(self.category, Category)
+
+
+class BookTestCase(TestCase):
+    def setUp(self):
+        self.category = Category.objects.create(name="test", )
+        self.book = Book.objects.create(title="Python", author="James", category=self.category)
+
+    def tearDown(self):
+        del self.category
+        del self.book
+
+    def test_book_instance(self):
+        self.assertIsInstance(self.book, Book)
+
+    def test_category_field(self):
+        self.assertIsInstance(self.book.category, Category)
+
+    def test_book_attributes(self):
+        self.assertEqual(self.book.title, 'Python')
+        self.assertEqual(self.book.author, 'James')
+
+    def test_string_representation(self):
+        self.assertEqual(str(self.book), 'Python by James')
