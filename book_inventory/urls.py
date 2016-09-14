a7e088e839545f9c9b40e57a6a07615aa5376895
andela-eisaac <deejecks@live.com>
2016-09-14 18:33:03 +0100

    Add url pattern for homepage

--- book_inventory/urls.py
+++ book_inventory/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 
+from book import views as book_views
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^$', book_views.HomePageView.as_view(), name='home'),
 ]
