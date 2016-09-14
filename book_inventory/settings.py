b519396eaeb2ed37330748d7813867be6fe15727
andela-eisaac <deejecks@live.com>
2016-09-14 18:01:46 +0100

    Include postgres db settings

--- book_inventory/settings.py
+++ book_inventory/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 
 import os
 
+import envvars
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -76,8 +78,15 @@ WSGI_APPLICATION = 'book_inventory.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': "bookinventory",
+        'USER': envvars.get("PGUSER"),
+        'PASSWORD': envvars.get('PGPASSWORD'),
+        'HOST': 'localhost',
+        'PORT': 5432,
+        'TEST': {
+            'NAME': 'test_bookinventory',
+        }
     }
 }
 
