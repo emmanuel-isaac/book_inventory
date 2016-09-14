bbc8bb90df1f5fae4308fb2f391b083c05300a05
andela-eisaac <deejecks@live.com>
2016-09-14 19:47:51 +0100

    Create static methods to perform search and use in post method

--- book/views.py
+++ book/views.py
@@ -3,9 +3,30 @@ from django.views.generic import View
 from django.http import HttpResponse
 
 from .models import Book
+from .forms import SearchForm
 
 
 class HomePageView(View):
+    books = Book.objects.all()
+    search_form = SearchForm()
+
     def get(self, request):
-        books = Book.objects.all()
-        return render(request, 'index.html', { 'books': books })
+        return render(request, 'index.html', {'books': self.books, 'search_form': self.search_form})
+
+    def post(self, request):
+        form = SearchForm(request.POST)
+        if form.is_valid():
+            search_term = form.data.get('search_term')
+            category_matches = list(self.get_category_matches(Book, search_term))
+            book_title_matches = list(self.get_book_title_matches(Book, search_term))
+            all_matches = list(set(category_matches + book_title_matches))
+        return render(request, 'index.html',
+                      {'matches': all_matches, 'books': self.books, 'search_form': self.search_form})
+
+    @staticmethod
+    def get_category_matches(model, search_term):
+        return getattr(model, 'objects').filter(category__name__contains=search_term)
+
+    @staticmethod
+    def get_book_title_matches(model, search_term):
+        return getattr(model, 'objects').filter(title__contains=search_term)
