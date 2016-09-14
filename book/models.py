83b0dc30ecebb739090a11ba7fb3089e15be5027
andela-eisaac <deejecks@live.com>
2016-09-14 18:19:21 +0100

    Create models for Book and Category models

--- book/models.py
+++ book/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=250, blank=False, null=False)
+
+    def __str__(self):
+        return "{}".format(self.name)
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=250, blank=False, null=False)
+    author = models.CharField(max_length=250, blank=False, null=False)
+    category = models.ForeignKey(Category)
+
+    def __str__(self):
+        return '{} by {}'.format(self.title, self.author)
